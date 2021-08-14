20611e5f579010066d15c0ce780e7f5250c2cc20
Denis Koshelev <truemacintoshchannel@gmail.com>
2021-08-14 15:41:37 +0300

    trying infinity polling out

--- main.py
+++ main.py
@@ -45,9 +45,10 @@ def start_command(message):
     bot.send_message(message.chat.id, "Привет, меня зовут Ваня! На самом деле, вонючка. Потому что воняю. Если тебе грустно, я подниму тебе настроение своей мордашкой!\n "
                                       "\nПока что я немного глупая и особо ничего не умею, но ты можешь написать мне что угодно, я тебя порадую. Правда моя мордашка придет не сразу, а секунд через пять, но как говорится: кто не терпит, тот не русский.")
 
+
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
-	bot.reply_to(message, message.text)
+    bot.reply_to(message, message.text)
 
 
 # @bot.message_handler(func=lambda m: True)
@@ -57,4 +58,5 @@ def echo_all(message):
 #     bot.send_photo(message.chat.id, photo=open(f"/Users/koselev/Desktop/vanya/{d}", "rb"))
 
 
-bot.polling()
+#bot.polling()
+bot.infinity_polling(True)
