2a033241f3d8fd1f709945f8b30b9b0e9a7f8814
Denis Koshelev <truemacintoshchannel@gmail.com>
2021-08-14 16:39:41 +0300

    added token bot to getMessage

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ if "HEROKU" in list(os.environ.keys()):
 
     server = Flask(__name__)
 
-    @server.route("/bot", methods=['POST'])
+    @server.route("/1935288146:AAEhOpae8frmTLSKwRxW80phpSWz3ikklpg", methods=['POST'])
     def getMessage():
         bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
         return "!", 200
