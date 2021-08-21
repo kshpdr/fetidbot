f2e6edc8780864751f3ba544d0688a99bc7baa54
Denis Koshelev <truemacintoshchannel@gmail.com>
2021-08-21 23:47:09 +0300

    Database attached and with heroku connected

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import os
 import telebot
 import random
+import psycopg2
 
+# environment for starting server and webhook
 from flask import Flask, request
 import logging
 
+# for taking comments out from the photos
 from PIL import Image
 
 #import requests
@@ -12,6 +15,11 @@ from PIL import Image
 
 bot = telebot.TeleBot('***REMOVED***')
 url = "https://api.telegram.org/bot***REMOVED***/"
+
+# conn = psycopg2.connect(database="fetidbot", user="denis", password="KatzeVanya", host="127.0.0.1", port="5432")
+DATABASE_URL = os.environ['postgresql-graceful-08047']
+conn =  psycopg2.connect(DATABASE_URL, sslmode='require')
+cur = conn.cursor()
 
 path = "photos"
 files = os.listdir(path)
@@ -45,12 +53,38 @@ files = os.listdir(path)
 #
 #         return last_update
 
+# inserting chat_id in data base
+def insert_user(chat_id, message):
+    cur.execute("INSERT INTO users (chat_id) \
+                VALUES (%d) \
+                ON CONFLICT (chat_id) DO NOTHING" % message.chat.id)
+    conn.commit()
 
-@bot.message_handler(commands=['start', 'help'])
+
+
+# inserting chat_id and photo_name in data base
+def insert_photo(chat_id, message, photo):
+    cur.execute("SELECT * FROM sent_photos")
+    # checking whether photo has been already sent
+    row = cur.fetchone()
+    while row is not None:
+        if row[1] == photo:
+            return False
+        row = cur.fetchone()
+
+    # if not, addr to database and update photos amount
+    cur.execute("INSERT INTO sent_photos (chat_id, photo_name)\
+                    VALUES (%d, '%s') \
+                ON CONFLICT ON CONSTRAINT no DO NOTHING" % (chat_id, photo))
+    cur.execute("UPDATE users SET total_photos = total_photos + 1 WHERE chat_id = '%d'" % chat_id)
+    conn.commit()
+    return True
+
+@bot.message_handler(commands=['start', 'help', 'hui'])
 def start_command(message):
     bot.send_message(message.chat.id, "Привет, меня зовут Ваня! На самом деле, вонючка. Потому что воняю. Если тебе грустно, я подниму тебе настроение своей мордашкой!\n "
                                       "\nПока что я немного глупая и особо ничего не умею, но ты можешь написать мне что угодно, я тебя порадую. Правда моя мордашка придет не сразу, а секунд через пять, но как говорится: кто не терпит, тот не русский.")
-
+    insert_user(message.chat.id, message)
 
 # @bot.message_handler(func=lambda m: True)
 # def echo_all(message):
@@ -59,10 +93,19 @@ def start_command(message):
 
 @bot.message_handler(func=lambda m: True)
 def send_photo(message):
+    cur.execute("SELECT * FROM users WHERE chat_id = %d" % message.chat.id)
+    row = cur.fetchone()
+    if row[1] == len(files):
+        bot.send_message(message.chat.id, "К сожалению, коллекция вонючкинса закончилась. Мы отправили запрос на пополнение и пришлем новую мордушка как только, так сразу!")
+        bot.send_message(206662948, "У кого-то закончился вонючкинс! Срочно контента!")
+        return
     d = random.choice(files)
-    comment = Image.open(f"photos/{d}").info["comment"].decode("utf-8")
-    bot.send_photo(message.chat.id, photo=open(f"photos/{d}", "rb"), caption=comment)
-
+    # recursively go through all photos to find a new one
+    if insert_photo(message.chat.id, message, d):
+        comment = Image.open(f"photos/{d}").info["comment"].decode("utf-8")
+        bot.send_photo(message.chat.id, photo=open(f"photos/{d}", "rb"), caption=comment)
+    else:
+        send_photo(message)
 
 # Проверим, есть ли переменная окружения Хероку (как ее добавить смотрите ниже)
 if "HEROKU" in list(os.environ.keys()):
