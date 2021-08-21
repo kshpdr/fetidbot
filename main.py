4991b2185831328669af64090e8bce31f9e8a837
Denis Koshelev <truemacintoshchannel@gmail.com>
2021-08-21 23:52:35 +0300

    psycopg-binary added

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from PIL import Image
 bot = telebot.TeleBot('1935288146:AAE7dB2WXQI-wdvrQlB9TY3wPFshpo0yCjE')
 url = "https://api.telegram.org/bot1935288146:AAE7dB2WXQI-wdvrQlB9TY3wPFshpo0yCjE/"
 
+# for local use
 # conn = psycopg2.connect(database="fetidbot", user="denis", password="KatzeVanya", host="127.0.0.1", port="5432")
 DATABASE_URL = os.environ['postgresql-graceful-08047']
 conn =  psycopg2.connect(DATABASE_URL, sslmode='require')
