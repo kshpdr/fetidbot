288eb8e95d9219f9cd0ba0c7483e99e6877e47e0
Denis Koshelev <truemacintoshchannel@gmail.com>
2021-08-25 19:04:49 +0300

    upload function added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import telebot
 import random
 import psycopg2
+import config
 
 # environment for starting server and webhook
 from flask import Flask, request
@@ -9,77 +10,27 @@ import logging
 
 # for taking comments out from the photos
 from PIL import Image
+import requests
+from imgurpython import ImgurClient
+import posixpath
+import urllib.parse
 
-#import requests
-#import datetime
+from database import insert_user, insert_photo
 
-bot = telebot.TeleBot('***REMOVED***')
-url = "https://api.telegram.org/bot***REMOVED***/"
+bot = telebot.TeleBot(config.telegram_token)
+url = config.telegram_url
 
 # for local use
 # conn = psycopg2.connect(database="fetidbot", user="denis", password="KatzeVanya", host="127.0.0.1", port="5432")
-DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+# DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(config.database_url, sslmode='require')
 cur = conn.cursor()
 
 path = "photos"
 files = os.listdir(path)
 
-# class BotHandler:
-#
-#     def __init__(self, token):
-#         self.token = token
-#         self.api_url = "https://api.telegram.org/bot{}/".format(token)
-#
-#     def get_updates(self, offset=None, timeout=30):
-#         method = 'getUpdates'
-#         params = {'timeout': timeout, 'offset': offset}
-#         resp = requests.get(self.api_url + method, params)
-#         result_json = resp.json()['result']
-#         return result_json
-#
-#     def send_message(self, chat_id, text):
-#         params = {'chat_id': chat_id, 'text': text}
-#         method = 'sendMessage'
-#         resp = requests.post(self.api_url + method, params)
-#         return resp
-#
-#     def get_last_update(self):
-#         get_result = self.get_updates()
-#
-#         if len(get_result) > 0:
-#             last_update = get_result[-1]
-#         else:
-#             last_update = get_result[len(get_result)]
-#
-#         return last_update
+client = ImgurClient(config.client_id, config.client_secret, config.access_token, config.refresh_token)
 
-# inserting chat_id in data base
-def insert_user(chat_id, message):
-    cur.execute("INSERT INTO users (chat_id) \
-                VALUES (%d) \
-                ON CONFLICT (chat_id) DO NOTHING" % message.chat.id)
-    conn.commit()
-
-
-
-# inserting chat_id and photo_name in data base
-def insert_photo(chat_id, message, photo):
-    cur.execute("SELECT * FROM sent_photos")
-    # checking whether photo has been already sent
-    row = cur.fetchone()
-    while row is not None:
-        if row[1] == photo and row[0] == chat_id:
-            return False
-        row = cur.fetchone()
-
-    # if not, addr to database and update photos amount
-    cur.execute("INSERT INTO sent_photos (chat_id, photo_name)\
-                    VALUES (%d, '%s') \
-                ON CONFLICT ON CONSTRAINT unique_photos DO NOTHING" % (chat_id, photo))
-    cur.execute("UPDATE users SET total_photos = total_photos + 1 WHERE chat_id = '%d'" % chat_id)
-    conn.commit()
-    return True
 
 @bot.message_handler(commands=['start', 'help', 'hui'])
 def start_command(message):
@@ -88,25 +39,64 @@ def start_command(message):
     insert_user(message.chat.id, message)
 
 # @bot.message_handler(func=lambda m: True)
-# def echo_all(message):
-#     bot.reply_to(message, message.text)
-
+# def send_photo(message):
+#     cur.execute("SELECT * FROM users WHERE chat_id = %d" % message.chat.id)
+#     row = cur.fetchone()
+#     print(row)
+#     if row[1] == len(files):
+#         bot.send_message(message.chat.id, "К сожалению, коллекция вонючкинса закончилась. Мы отправили запрос на пополнение и пришлем новую мордушка как только, так сразу!")
+#         bot.send_message(206662948, "У кого-то закончился вонючкинс! Срочно контента!")
+#         return
+#     d = random.choice(files)
+#     while d == ".DS_Store":
+#         d = random.choice(files)
+#     print(d)
+#     # recursively go through all photos to find a new one
+#     if insert_photo(message.chat.id, message, d):
+#         comment = Image.open(f"photos/{d}").info["comment"].decode("utf-8")
+#         bot.send_photo(message.chat.id, photo=open(f"photos/{d}", "rb"), caption=comment)
+#     else:
+#         send_photo(message)
 
 @bot.message_handler(func=lambda m: True)
 def send_photo(message):
+    # check how many photos have been already sent
     cur.execute("SELECT * FROM users WHERE chat_id = %d" % message.chat.id)
     row = cur.fetchone()
-    if row[1] == len(files):
+    cur.execute("SELECT COUNT(*) FROM photos")
+    total = cur.fetchone()[0]  # number of photos
+    # if it matches with amount of files, then a corresponding message
+    if row[1] >= total:
         bot.send_message(message.chat.id, "К сожалению, коллекция вонючкинса закончилась. Мы отправили запрос на пополнение и пришлем новую мордушка как только, так сразу!")
         bot.send_message(206662948, "У кого-то закончился вонючкинс! Срочно контента!")
         return
-    d = random.choice(files)
+    # otherwise choose a random photo
+    cur.execute("SELECT * FROM photos ORDER BY RANDOM() LIMIT 1")
+    row = cur.fetchone()
+    photo_link = row[0]
     # recursively go through all photos to find a new one
-    if insert_photo(message.chat.id, message, d):
-        comment = Image.open(f"photos/{d}").info["comment"].decode("utf-8")
-        bot.send_photo(message.chat.id, photo=open(f"photos/{d}", "rb"), caption=comment)
+    if insert_photo(message.chat.id, message, photo_link):
+        comment = row[1]
+        bot.send_photo(message.chat.id, photo=photo_link, caption=comment)
     else:
         send_photo(message)
+
+
+@bot.message_handler(func=lambda message: message.chat.id == 206662948 or message.chat.id == 189636044, content_types=['photo'])
+def upload_photo(message):
+    file_id = message.photo[-1].file_id
+    file_info = bot.get_file(file_id)
+    file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(config.telegram_token, file_info.file_path))
+    file_url = 'https://api.telegram.org/file/bot{0}/{1}'.format(config.telegram_token, file_info.file_path)
+
+    configs = {
+        'album': config.album_id,
+        'name': 'fetidbot',
+    }
+    image = client.upload_from_url(file_url, config=configs, anon=False)
+    cur.execute("INSERT INTO photos VALUES ('%s', '%s');" % (image['link'], message.caption))
+    conn.commit()
+    bot.send_message(message.chat.id, "Архив вонючкинсов пополнен!")
 
 # Проверим, есть ли переменная окружения Хероку (как ее добавить смотрите ниже)
 if "HEROKU" in list(os.environ.keys()):
@@ -123,7 +113,7 @@ if "HEROKU" in list(os.environ.keys()):
     @server.route("/")
     def webhook():
         bot.remove_webhook()
-        bot.set_webhook(url="https://vast-ravine-49209.herokuapp.com") # этот url нужно заменить на url вашего Хероку приложения
+        bot.set_webhook(url=config.app_url) # этот url нужно заменить на url вашего Хероку приложения
         return "?", 200
     server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
 else:
@@ -133,3 +123,4 @@ else:
     bot.polling(none_stop=True)
 
 #bot.polling()
+
