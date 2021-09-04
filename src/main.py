bc075860e812f27fdf85e728b1dac0b83593a686
Denis Koshelev <truemacintoshchannel@gmail.com>
2021-09-04 23:25:03 +0300

    Happy Birthday Edition

--- src/main.py
+++ src/main.py
@@ -17,7 +17,17 @@ from imgurpython import ImgurClient
 import posixpath
 import urllib.parse
 
+# for uploading and downloading files from/to google drive
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+# local imports from other files
 from database import insert_user, insert_photo
+
+# for scheduled message
+from threading import Thread
+import schedule
+import birthday_congrats as bc
 
 bot = telebot.TeleBot(config.telegram_token)
 url = config.telegram_url
@@ -32,12 +42,85 @@ cur = conn.cursor()
 
 client = ImgurClient(config.client_id, config.client_secret, config.access_token, config.refresh_token)
 
+# gauth = GoogleAuth()
+# gauth.LocalWebserverAuth()
+# drive = GoogleDrive(gauth)
+counter = 0
 
-@bot.message_handler(commands=['start', 'help', 'hui'])
+# handling a keyboard answer
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    global counter
+    if call.data == "further":
+        bot.answer_callback_query(call.id, "Ну погнали")
+        if counter == 1:
+            bc.first_message()
+            counter += 1
+        elif counter == 2:
+            bc.second_message()
+            counter += 1
+        elif counter == 3:
+            bc.third_message()
+            counter += 1
+        elif counter == 4:
+            bc.fourth_message()
+            counter += 1
+        elif counter == 5:
+            bc.fifth_message()
+            counter += 1
+        elif counter == 6:
+            bc.sixth_message()
+            counter += 1
+        elif counter == 7:
+            bc.seventh_message()
+            counter += 1
+        elif counter == 8:
+            bc.eighth_message()
+            counter += 1
+        elif counter == 9:
+            bc.ninth_message()
+            counter += 1
+        elif counter == 10:
+            bc.tenth_message()
+            counter += 1
+        elif counter == 11:
+            bc.eleventh_message()
+            counter += 1
+        elif counter == 12:
+            bc.twelveth_message()
+            counter += 1
+        elif counter == 13:
+            bc.thirteenth_message()
+            counter += 1
+        elif counter == 14:
+            bc.fourteenth_message()
+            counter += 1
+    elif call.data == "fake_present":
+        bot.answer_callback_query(call.id, "Че, реально подарок?")
+    #     if counter == 15:
+    #         bc.present_fake()
+    #         counter += 1
+    # elif call.data == "real_present":
+    #     bot.answer_callback_query(call.id, "А щас че тогда?")
+    #     if counter == 16:
+    #         bc.present_real()
+    #         counter += 1
+
+
+
+@bot.message_handler(commands=['start', 'help'])
 def start_command(message):
     bot.send_message(message.chat.id, "Привет, меня зовут Ваня! На самом деле, вонючка. Потому что воняю. Если тебе грустно, я подниму тебе настроение своей мордашкой!\n "
                                       "\nПока что я немного глупая и особо ничего не умею, но ты можешь написать мне что угодно, я тебя порадую. Правда моя мордашка придет не сразу, а секунд через пять, но как говорится: кто не терпит, тот не русский.")
     insert_user(message.chat.id, message)
+
+
+@bot.message_handler(commands=['oldbutgold'])
+def birthday(message):
+    comment = "Чтобы скушно не было, будем разбавлять мои гениальные сообщения не менее гениальными фотографиями! На этой например я успешно парадирую вонючку..."
+    bot.send_photo(206662948, photo="https://imgur.com/rNFXNOB", caption=comment, reply_markup=bc.gen_markup())
+    global counter
+    counter = 1
 
 
 @bot.message_handler(func=lambda m: True)
@@ -88,6 +171,22 @@ def upload_photo(message):
     cur.execute("INSERT INTO photos VALUES ('%s', '%s');" % (image['link'], message.caption))
     conn.commit()
     bot.send_message(message.chat.id, "Архив вонючкинсов пополнен!")
+
+
+@bot.message_handler(func=lambda message: message.chat.id == 206662948, content_types=['voice'])
+def upload_voice(message):
+    file_id = message.voice.file_id
+    file_info = bot.get_file(file_id)
+    file_url = 'https://api.telegram.org/file/bot{0}/{1}'.format(config.telegram_token, file_info.file_path)
+    voice = drive.CreateFile({'parents': [{'id': '1q76yX9lg0UzYORBOqKXlUuUh7dHCmsgn'}], 'title': file_info.file_path, 'download_url': file_url})
+    voice.FetchMetadata()
+    voice.FetchContent()
+    voice.Upload()
+
+
+# Setup for scheduled messages
+schedule.every().day.at("23:26").do(bc.start_birthday)
+Thread(target=bc.schedule_checker).start()
 
 
 # check if heroku variable is in the environment
